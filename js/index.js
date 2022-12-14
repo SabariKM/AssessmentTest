7c4c115610f1a248f3fc8ebe8358725219442123
SabariKM <113346395+SabariKM@users.noreply.github.com>
2022-12-15 00:57:01 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 const richardsontimeZone = document.querySelector(".richardsontimeZone");
 const indiaTimeZone = document.querySelector(".indiaTimeZone");
 const date = new Date();
-console.log(date)
 
 const richardsonDate = date.toLocaleString('en-US', {timeZone:'CST'});
 const indiaDate = date.toLocaleString('en-US', {timeZone:'IST'});
 
 indiaTimeZone.innerHTML = "India Time : " + indiaDate;
 richardsontimeZone.innerHTML = "Richardson Time : " + richardsonDate;
+console.log("End");
